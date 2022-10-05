c48cf8be15b3b64d77c92597e49e2f536fe4b6d0
samArtDev <samthe3dartist@gmail.com>
2022-10-04 20:04:58 -0500

    final pseudocode

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ uint8_t upTruthTable[4] = {1, 1, 0, 1};
 uint8_t rightTruthTable[4] = {1, 1, 1, 0};
 uint8_t downTruthTable[4] = {0, 1, 1, 1};
 uint8_t leftTruthTable[4] = {1, 0, 1, 1};
+uint8_t emptyTruthTable[4] = {0, 0, 0, 0};
 
 uint8_t totalNumbOfTiles = 4;
 
@@ -18,17 +19,93 @@ int8_t centerX = 10;
 int8_t centerY = 9;
 int8_t tempX;
 int8_t tempY;
-uint8_t centerTile[4];
-uint8_t tempArray[4];
+uint8_t centerTile;
+uint8_t placeholderCenterTile[4];
+uint8_t placeholderRandArray[4];
+
+void setPosition(uint8_t x, uint8_t y){
+    centerX = x;
+    centerY = y;
+}
 
 uint8_t rollRNG(){
     uint8_t randomNumberIndex = rand();
     randomNumberIndex = randomNumberIndex % 4;
+    assignTruthTable(placeholderRandArray[4], randomNumberIndex);
     return(randomNumberIndex);
 }
 
 void setCenterTile(uint8_t centerX, uint8_t centerY, uint8_t centerTile){
+    assignTruthTable(placeholderCenterTile, centerTile);
     set_bkg_tile_xy(centerX, centerY, centerTile);
+}
+
+void assignTruthTable(uint8_t arrayToBeAssigned[4], uint8_t index){
+
+    uint8_t arrayToBeAssigned[4] = {0, 0, 0, 0};
+
+    switch (index)
+    {
+    case 0:
+        for (uint8_t i = 0; i < 3; i++)
+        {
+            arrayToBeAssigned[i] = blankTruthTable[i];
+        }
+        break;
+
+    case 1:
+        for (uint8_t i = 0; i < 3; i++)
+        {
+            arrayToBeAssigned[i] = upTruthTable[i];
+        }
+        break;
+
+    case 2:
+        for (uint8_t i = 0; i < 3; i++)
+        {
+            arrayToBeAssigned[i] = rightTruthTable[i];
+        }
+        break;
+    
+    case 3:
+        for (uint8_t i = 0; i < 3; i++)
+        {
+            arrayToBeAssigned[i] = downTruthTable[i];
+        }
+        break;
+    
+    case 4:
+        for (uint8_t i = 0; i < 3; i++)
+        {
+            arrayToBeAssigned[i] = leftTruthTable[i];
+        }
+        break;
+
+    case 5:
+        for (uint8_t i = 0; i < 3; i++)
+        {
+            arrayToBeAssigned[i] = emptyTruthTable[i];
+        }
+        break;
+    }
+}
+
+void placeTile(){
+    uint8_t RNGval = rollRNG();
+
+    if(isN_Good == true){
+        if(placeholderCenterTile[0] != placeholderRandArray[2]){
+            rollRNG();
+            placeTile();
+        }
+        if(placeholderCenterTile[1] != placeholderRandArray[3]){
+            rollRNG();
+            placeTile();
+        }
+        else{
+            set_bkg_data(tempX, tempY, RNGval);
+        }
+    }
 }
 
 void isN_Good(){
@@ -41,7 +118,8 @@ void isN_Good(){
     
     if(get_bkg_tile_xy(tempX, tempY) == 0){
         return true;
-    } 
+        placeTile(tempX, tempY);
+    }
 }
 
 void isE_Good(){
@@ -54,6 +132,7 @@ void isE_Good(){
     
     if(get_bkg_tile_xy(tempX, tempY) == 0){
         return true;
+        placeTile(tempX, tempY);
     } 
 }
 
@@ -67,6 +146,7 @@ void isW_Good(){
     
     if(get_bkg_tile_xy(tempX, tempY) == 0){
         return true;
+        placeTile(tempX, tempY);
     } 
 }
 
@@ -80,6 +160,7 @@ void isS_Good(){
     
     if(get_bkg_tile_xy(tempX, tempY) == 0){
         return true;
+        placeTile(tempX, tempY);
     } 
 }
 
@@ -96,8 +177,6 @@ void main(){
         isS_Good();
 
     }
-    
-    setN_E_W_S();
     delay(1000);
     SHOW_BKG;
     DISPLAY_ON;
